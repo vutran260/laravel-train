5edffcca196a16722ce8839f1fbfed133b51147e
dcson <dcson@cmcglobal.vn>
2023-04-03 17:21:12 +0700

    update

--- routes/web.php
+++ routes/web.php
@@ -95,3 +95,11 @@ Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name
 Auth::routes();
 
 Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
+
+Auth::routes();
+
+Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
+
+Auth::routes();
+
+Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
